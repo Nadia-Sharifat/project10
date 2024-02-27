ce382f3e77c943977bbc9008cc317e721133dcbc
Nadia Sharifat <nadia@outlook.com>
2024-02-27 22:25:23 +0330

    v1.5

--- project10.sh
+++ project10.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+read -p "Enter container name:  " CONTAINER_NAME
 
 # Step 1: Run the Ubuntu container
-container_id=$(docker run -dit --name ubuntu10 --expose 22 ubuntu)
+CONTAINER_ID=$(docker run -dit --name $CONTAINER_NAME --expose 22 ubuntu)
 
 # Step 2: Install OpenSSH server
-docker exec $container_id apt update
-docker exec $container_id apt install -y openssh-server
+docker exec $CONTAINER_ID apt update
+docker exec $CONTAINER_ID apt install -y openssh-server
 
 # Step 3: Start SSH service
-docker exec $container_id service ssh start
+docker exec $CONTAINER_ID service ssh start
 
 # Step 4: Add user 'nadia' to the '_ssh' group
-docker exec $container_id useradd nadia -G _ssh
+docker exec $CONTAINER_ID useradd nadia -G _ssh
 
 # Step 5: Retrieve container IP address
-container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
+CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
 
 # Step 6: SSH into the container as root to set nadia's password
-ssh root@$container_ip
+ssh root@$CONTAINER_IP
 
 # Step 7: Set password for 'nadia'
-docker exec -it $container_id passwd nadia
+docker exec -it $CONTAINER_ID passwd nadia
 
 # Step 8: Exit from SSH session
 exit
 
 # Step 9: SSH into the container as 'nadia'
-ssh nadia@$container_ip
+ssh nadia@$CONTAINER_IP
 
